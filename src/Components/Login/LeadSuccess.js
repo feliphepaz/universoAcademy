dcb9a4bc61eec255815d7d48f308e66dbf9c6072
Feliphe Paz <feliphe.s.paz@gmail.com>
2021-11-25 10:03:34 -0300

    componente de sucesso no envio da lead feito

---
+++ src/Components/Login/LeadSuccess.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeadSuccess = () => {
+    return (
+        <section className='login-nav animate' data-anime='right'>
+            <h2>Obrigado!</h2>
+        </section>
+    )
+}
+
+export default LeadSuccess
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router';
+import { Routes, Route } from 'react-router';
 import './Login.css';
 import Lead from './Lead';
+import LeadSuccess from './LeadSuccess';
 
 const Login = () => {
     return (
@@ -12,6 +13,7 @@ const Login = () => {
                     {/* <Route path='/' element={<LoginForm />} />
                     <Route path='cadastrar' element={<LoginCreate />} /> */}
                     <Route path='assine' element={<Lead />} />
+                    <Route path='sucesso' element={<LeadSuccess />} />
                     {/* <Route path='perdi-senha' element={<LoginPasswordLost />} />
                     <Route path='resetar-senha' element={<LoginPasswordReset />} /> */}
                 </Routes>
